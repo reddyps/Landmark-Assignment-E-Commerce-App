5623d8a95fe9a29a72e5a22a318c892968f3858c
Naveen Kumar P S <reddyspanveenkumar@gmail.com>
2025-05-25 21:28:38 +0530

    test case and payment

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -47,4 +47,5 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.android.material:material:1.12.0")
 }
