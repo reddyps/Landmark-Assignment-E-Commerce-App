a4f914eb5ac05d542b6efb9cf769157270020aab
Naveen Kumar Reddy PS <reddypsnaveenkumar@gmail.com>
2025-05-26 10:09:47 +0530

    fixes

--- test/features/product/views/products_screen_test.dart
+++ test/features/product/views/products_screen_test.dart
@@ -1,6 +1,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:landmark_assignment/features/product/views/components/offer_carousel_and_categories.dart';
+import 'package:landmark_assignment/features/product/views/screens/categories_products_all.dart';
 import 'package:mockito/mockito.dart';
 import 'package:landmark_assignment/features/product/views/products_screen.dart';
 import 'package:landmark_assignment/features/product/viewmodels/products_view_model.dart';
@@ -15,8 +17,8 @@ void main() {
       ),
     );
 
-    expect(find.byType(ProductsScreen), findsOneWidget);
-    expect(find.byType(CustomScrollView), findsOneWidget);
+    expect(find.byType(CategoriesProducts), findsOneWidget);
+    expect(find.byType(OffersCarouselAndCategories), findsOneWidget);
     expect(find.byType(SliverToBoxAdapter), findsWidgets);
   });
 }
